efe34f765488dfa15c1be5767d25ec1263f599a3
Linlin <hiliulin@aliyun.com>
2018-11-11 18:16:44 +0800

    增加友情链接管理

---
+++ src/config/friend.js
@@ -0,0 +1,77 @@
+/**
+ * Created by WebStorm.
+ * User: liulin
+ * Date: 2018/11/10
+ * Time: 11:47 PM
+ */
+
+import form from '../const/form'
+
+export default {
+  apis: [
+    {
+      type: 'all', // 类型，用于区分是什么类型的接口
+      name: 'link.all', // 接口字符串
+      params: [
+        {
+          name: 'type',
+          type: String,
+          default: 'link'
+        }
+      ]
+    },
+    {
+      type: 'update',
+      name: 'link.add'
+    }
+  ],
+  columns: [
+    {
+      type: 'index',
+      width: 60,
+      align: 'center'
+    },
+    {
+      title: '名称',
+      key: 'nickname'
+    },
+    {
+      title: '描述',
+      key: 'description'
+    },
+    {
+      title: 'URL',
+      key: 'link'
+    },
+    {
+      title: '颜色',
+      key: 'color'
+    },
+    {
+      title: '创建时间',
+      key: 'create_time'
+    }
+  ],
+  form: [
+    {
+      key: 'nickname',
+      label: '标签名称:',
+      type: form.FORM_TYPE_STRING
+    },
+    {
+      key: 'description',
+      label: '描述:',
+      type: form.FORM_TYPE_STRING
+    },
+    {
+      key: 'link',
+      label: '连接:',
+      type: form.FORM_TYPE_STRING
+    },
+    {
+      key: 'color',
+      label: '标签颜色:',
+      type: form.FORM_TYPE_STRING
+    }
+  ]
+}
--- src/pages/Friend.vue
+++ src/pages/Friend.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>这里是友链列表</div>
+  <div class="article-list">
+    <CommonTable
+      add-title="友链"
+      :table-config="config"/>
+  </div>
 </template>
 
 <script>
+import CommonTable from '../components/commonTable/CommonTable'
+import friendConfig from '../config/friend'
+import { Store } from '../common'
 export default {
-  name: 'ArticleTag'
+  name: 'Article',
+  components: {
+    CommonTable
+  },
+  data () {
+    return {
+      config: friendConfig
+    }
+  },
+  created () {
+    Store.store('curConfig', this.config)
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .article-list {
+    text-align: left;
+    height: 100%;
+  }
 </style>

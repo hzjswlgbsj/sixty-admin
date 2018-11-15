1bd9b69906d981ec0815c7e51fa52e9caf7cb0b7
Linlin <hiliulin@aliyun.com>
2018-11-15 09:16:01 +0800

    增加评论配置文件

---
+++ src/config/comment.js
@@ -0,0 +1,88 @@
+/**
+ * Created by WebStorm.
+ * User: liulin
+ * Date: 2018/11/15
+ * Time: 9:12 AM
+ */
+import form from '../const/form'
+
+export default {
+  apis: [
+    {
+      type: 'all', // 类型，用于区分是什么类型的接口
+      name: 'link.all', // 接口字符串
+      params: [
+        {
+          name: 'type',
+          type: String,
+          default: 'link'
+        }
+      ]
+    },
+    {
+      type: 'update',
+      name: 'link.add'
+    }
+  ],
+  columns: [
+    {
+      type: 'index',
+      width: 60,
+      align: 'center'
+    },
+    {
+      title: '名称',
+      key: 'nickname'
+    },
+    {
+      title: '描述',
+      key: 'description'
+    },
+    {
+      title: 'URL',
+      key: 'link'
+    },
+    {
+      title: '颜色',
+      key: 'color'
+    },
+    {
+      title: '创建时间',
+      key: 'create_time'
+    }
+  ],
+  form: [
+    {
+      key: 'nickname',
+      label: '标签名称:',
+      type: form.FORM_TYPE_STRING
+    },
+    {
+      key: 'description',
+      label: '描述:',
+      type: form.FORM_TYPE_STRING
+    },
+    {
+      key: 'link',
+      label: '连接:',
+      type: form.FORM_TYPE_STRING
+    },
+    {
+      key: 'color',
+      label: '标签颜色:',
+      type: form.FORM_TYPE_ENUM,
+      options: [
+        {label: '紫色', value: 'purple'},
+        {label: '蓝色', value: 'blue'},
+        {label: '白色', value: 'white'},
+        {label: '黄色', value: 'yellow'},
+        {label: '绿色', value: 'green'}
+      ]
+    },
+    {
+      key: 'logo_url',
+      label: 'logo链接:',
+      type: form.FORM_TYPE_STRING
+    }
+  ]
+}
--- src/pages/ArticleComment.vue
+++ src/pages/ArticleComment.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>这里是文章评论</div>
+  <div class="article-list">
+    <CommonTable
+      add-title="评论列表"
+      :table-config="config"/>
+  </div>
 </template>
 
 <script>
+import CommonTable from '../components/commonTable/CommonTable'
+import commentConfig from '../config/comment'
+import { Store } from '../common'
 export default {
-  name: 'ArticleComment'
+  name: 'ArticleComment',
+  components: {
+    CommonTable
+  },
+  data () {
+    return {
+      config: commentConfig
+    }
+  },
+  created () {
+    Store.store('curConfig', this.config)
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .article-list {
+    text-align: left;
+    height: 100%;
+  }
 </style>

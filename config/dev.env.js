01ab8798e20be49192dc5afd2a45cd2cdea90da1
Linlin <hiliulin@aliyun.com>
2018-12-03 17:07:58 +0800

    修改api的主机，修改头像

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_DOMAIN: '"http://api.sixty.com"',
+  API_DOMAIN: '"https://api.sixtyden.com"',
   WEIBO_DOMAIN: '"https://api.weibo.com"',
   WEIBO_GET_TOKEN_URL: '"https://api.weibo.com/oauth2/access_token"',
   WEIBO_GET_USER_URL: '"https://api.weibo.com/2/users/show.json"'
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_DOMAIN: '"http://api.sixtyden.com"',
+  API_DOMAIN: '"https://api.sixtyden.com"',
   WEIBO_DOMAIN: '"https://api.weibo.com"',
   WEIBO_GET_TOKEN_URL: '"https://api.weibo.com/oauth2/access_token"',
   WEIBO_GET_USER_URL: '"https://api.weibo.com/2/users/show.json"'
